495cd181511d6545232c25ce18d83e33822c530a
Júlio César de Brito Gardona <jcbritobr@gmail.com>
2021-03-10 09:35:14 -0300

    All initialization and clean up implemented.

--- nvmllib/Nvml.cs
+++ nvmllib/Nvml.cs
@@ -9,6 +9,8 @@ namespace Nvidia.Nvml
 
         [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlInit")]
         internal static extern NvmlReturn NvmlInit();
+        [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlInitWithFlags")]
+        internal static extern NvmlReturn NvmlInitWithFlags(uint flags);
         [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlInit_v2")]
         internal static extern NvmlReturn NvmlInitV2();
         [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlDeviceGetHandleByIndex")]
@@ -17,10 +19,33 @@ namespace Nvidia.Nvml
         internal static extern NvmlReturn NvmlDeviceGetTemperature(IntPtr device, NvmlTemperatureSensor sensorType, out uint temperature);
         [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlShutdown")]
         internal static extern NvmlReturn NvmlShutdown();
+        [DllImport(NvmlSharedLibrary, EntryPoint = "nvmlSystemGetCudaDriverVersion")]
+        internal static extern NvmlReturn NvmlSystemGetCudaDriverVersion(out int cudaDriverVersion);
     }
 
     public class NvGpu
     {
+        public const uint NVML_INIT_FLAG_NO_GPUS = 1;
+        public const uint NVML_INIT_FLAG_NO_ATTACH = 2;
+
+        public static int CudaDriverVersionMajor(int version)
+        {
+            return version / 1000;
+        }
+
+        public static int NvmlSystemGetCudaDriverVersion()
+        {
+            int driverVersion;
+            NvmlReturn res;
+            res = Api.NvmlSystemGetCudaDriverVersion(out driverVersion);
+            if (NvmlReturn.NVML_SUCCESS != res)
+            {
+                throw new SystemException(res.ToString());
+            }
+
+            return driverVersion;
+        }
+
         public static void NvmlInitV2()
         {
             NvmlReturn res;
@@ -31,14 +56,60 @@ namespace Nvidia.Nvml
             }
         }
 
-        public static void NvmlShutdown()
+        public static void NvmlInit()
         {
             NvmlReturn res;
-            res = Api.NvmlInitV2();
+            res = Api.NvmlInit();
             if (NvmlReturn.NVML_SUCCESS != res)
             {
                 throw new SystemException(res.ToString());
             }
+        }
+
+        public static void NvmlInitWithFlags(uint flags)
+        {
+            NvmlReturn res;
+            res = Api.NvmlInitWithFlags(flags);
+            if (NvmlReturn.NVML_SUCCESS != res)
+            {
+                throw new SystemException(res.ToString());
+            }
+        }
+
+        public static void NvmlShutdown()
+        {
+            NvmlReturn res;
+            res = Api.NvmlShutdown();
+            if (NvmlReturn.NVML_SUCCESS != res)
+            {
+                throw new SystemException(res.ToString());
+            }
+        }
+
+        public static IntPtr NvmlDeviceGetHandleByIndex(uint index)
+        {
+            var device = IntPtr.Zero;
+            NvmlReturn res;
+            res = Api.NvmlDeviceGetHandleByIndex(index, out device);
+            if (NvmlReturn.NVML_SUCCESS != res)
+            {
+                throw new SystemException(res.ToString());
+            }
+
+            return device;
+        }
+
+        public static uint NvmlDeviceGetTemperature(IntPtr device, NvmlTemperatureSensor sensorType)
+        {
+            NvmlReturn res;
+            uint temperature;
+            res = Api.NvmlDeviceGetTemperature(device, sensorType, out temperature);
+            if (NvmlReturn.NVML_SUCCESS != res)
+            {
+                throw new SystemException(res.ToString());
+            }
+
+            return (uint)temperature;
         }
     }
 }
--- nvmltests/NvmlTest.cs
+++ nvmltests/NvmlTest.cs
@@ -12,18 +12,83 @@ namespace NvlmTests
         }
 
         [Test]
-        public void NvmlInitializationTest()
+        public void RetriveCudaDriverVersion()
+        {
+            try
+            {
+                NvGpu.NvmlInitV2();
+                int version = NvGpu.NvmlSystemGetCudaDriverVersion();
+                int major = NvGpu.CudaDriverVersionMajor(version);
+                NvGpu.NvmlShutdown();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        [Test]
+        public void InitializationTest()
+        {
+            try
+            {
+                NvGpu.NvmlInit();
+                NvGpu.NvmlShutdown();
+                NvGpu.NvmlInitV2();
+                NvGpu.NvmlShutdown();
+                NvGpu.NvmlInitWithFlags(NvGpu.NVML_INIT_FLAG_NO_ATTACH);
+                NvGpu.NvmlShutdown();
+                NvGpu.NvmlInitWithFlags(NvGpu.NVML_INIT_FLAG_NO_GPUS);
+                NvGpu.NvmlShutdown();
+                NvGpu.NvmlInitWithFlags(NvGpu.NVML_INIT_FLAG_NO_GPUS | NvGpu.NVML_INIT_FLAG_NO_ATTACH);
+                NvGpu.NvmlShutdown();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [Test]
+        public void RecoverDevice()
         {
             try
             {
                 var device = IntPtr.Zero;
                 NvGpu.NvmlInitV2();
+                device = NvGpu.NvmlDeviceGetHandleByIndex(0);
+                if (IntPtr.Zero == device)
+                {
+                    Assert.Fail("Device cant be IntPtr.Zero");
+                }
                 NvGpu.NvmlShutdown();
             }
-            catch (System.Exception)
+            catch (Exception e)
             {
-                throw;
+                Assert.Fail(e.Message);
             }
+        }
+
+        [Test]
+        public void GetGpuTemperature()
+        {
+            try
+            {
+                var device = IntPtr.Zero;
+                NvGpu.NvmlInitV2();
+                device = NvGpu.NvmlDeviceGetHandleByIndex(0);
+                var temperature = NvGpu.NvmlDeviceGetTemperature(device, NvmlTemperatureSensor.NVML_TEMPERATURE_GPU);
+                if (!(temperature >= 40 && temperature <= 80))
+                {
+                    Assert.Fail("Cant get temperature");
+                }
+                NvGpu.NvmlShutdown();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.ToString());
+            }
+
         }
     }
 }
